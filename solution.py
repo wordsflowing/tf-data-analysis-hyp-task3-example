e4f113c9bdbad13d2358da541b442eee7d6e1bb4
wordsflowing <129582590+wordsflowing@users.noreply.github.com>
2023-04-11 15:56:36 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as stats
 
 
 chat_id = 422119389 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x, y) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    return stats.ttest_ind(x, y)[1] < 0.09 # Ваш ответ, True или False
